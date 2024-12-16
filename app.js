0b3e8561a01e5d12c4caac94b8b34792837a8b1d
Akshat Kumar <108181558+Akshatk871@users.noreply.github.com>
2024-12-17 00:42:27 +0530

    Changed Secret

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(session({
-   secret:"Our little secret.",
+   secret:"Our secret.",
    resave:false,
    saveUninitialized:false 
 }));
